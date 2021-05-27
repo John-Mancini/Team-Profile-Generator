f2c5d8599108863eca72deafd5b3517bdd33c8ca
John-Mancini <mancinijohn5988@gmail.com>
2021-05-27 13:50:57 -0400

    added all 3 functions

---
+++ server.js
@@ -0,0 +1,100 @@
+//imports
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const employees = [];
+
+function engineerInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of this Engineer?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the ID of this Engineer?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the Email of this Engineer?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the Github of this Engineer?",
+      },
+    ])
+    .then((answers) => {
+      var { name, id, email, github } = answers;
+      var engineer = Engineer(name, id, email, github);
+      employees.push(engineer);
+    });
+}
+
+function internInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of this intern?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the ID of this intern?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the Email of this intern?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the School of this intern?",
+      },
+    ])
+    .then((answers) => {
+      var { name, id, email, school } = answers;
+      var intern = Intern(name, id, email, school);
+      employees.push(intern);
+    });
+}
+function managerInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of this manager?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the ID of this manager?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the Email of this manager?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the Office Number of this manager?",
+      },
+    ])
+    .then((answers) => {
+      var { name, id, email, officeNumber } = answers;
+      var manager = Manager(name, id, email, officeNumber);
+      employees.push(manager);
+    });
+}
